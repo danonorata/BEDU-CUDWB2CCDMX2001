22cccdb5ec4f14e2b2f8555f8af2e11cee00953c
danonorata <60279745+danonorata@users.noreply.github.com>
2020-04-25 12:34:04 -0500

    Cambio a Función
    Cambié a &

--- RetoFizzBuzz.js
+++ RetoFizzBuzz.js
@@ -1,15 +1,12 @@
-var analizar = 15;
+var analizar = 27;
 
-if (analizar % 3 === 0 || analizar % 5 === 0) {
-if (analizar % 3 === 0) {
-    console.info("Fizz");
-}
-if (analizar % 5 === 0) {
+
+if (analizar % 3 === 0 & analizar % 5 === 0) {
+    console.info("FizzBuzz");
+} else if (analizar % 5 === 0) {
     console.info("Buzz");
-}
-if (analizar % 3 === 0 || analizar % 5 === 0) {
-    console.log("FizzBuzz");
-}
-else {
+} else if (analizar % 3 === 0 ) {
+    console.log("Fizz");
+} else {
     console.warn("No hay compatibilidad");
-}}
+}
